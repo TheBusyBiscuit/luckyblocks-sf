5951ba3d52fa6dbc12e911389bd35e1e858492c0
LinoxGH <denizcanaksu121@gmail.com>
2020-06-28 23:05:48 +0300

    Updated to 1.16

--- src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/unlucky/ZombiePigmenSurprise.java
+++ src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/unlucky/ZombiePigmenSurprise.java
@@ -7,6 +7,8 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.PigZombie;
 import org.bukkit.entity.Player;
 
+import io.github.thebusybiscuit.slimefun4.api.MinecraftVersion;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
 import io.github.thebusybiscuit.slimefunluckyblocks.surprises.LuckLevel;
 import io.github.thebusybiscuit.slimefunluckyblocks.surprises.Surprise;
 
@@ -20,7 +22,8 @@ public final class ZombiePigmenSurprise implements Surprise {
 	@Override
 	public void activate(Random random, Player p, Location l) {
 		for (int i = 0; i < 4; i++) {
-			PigZombie pigman = (PigZombie) l.getWorld().spawnEntity(l.add(random.nextInt(4) - (double) random.nextInt(8), 1, random.nextInt(4) - (double) random.nextInt(8)), EntityType.PIG_ZOMBIE);
+			EntityType type = SlimefunPlugin.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_16) ? EntityType.ZOMBIFIED_PIGLIN : EntityType.valueOf("PIG_ZOMBIE");
+			PigZombie pigman = (PigZombie) l.getWorld().spawnEntity(l.add(random.nextInt(4) - (double) random.nextInt(8), 1, random.nextInt(4) - (double) random.nextInt(8)), type);
 			pigman.setAngry(true);
 			pigman.setTarget(p);
 		}
