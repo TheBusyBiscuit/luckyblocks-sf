c2c2cc76a376490c52beb9b8a7983ca92a286913
TheBusyBiscuit <mrCookieSlime@gmail.com>
2021-01-18 17:17:07 +0100

    Fixed #36

--- src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/lucky/DiamondBlockPillarSurprise.java
+++ src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/lucky/DiamondBlockPillarSurprise.java
@@ -6,9 +6,9 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
+import io.github.thebusybiscuit.slimefun4.utils.ColoredMaterial;
 import io.github.thebusybiscuit.slimefunluckyblocks.surprises.LuckLevel;
 import io.github.thebusybiscuit.slimefunluckyblocks.surprises.Surprise;
-import me.mrCookieSlime.Slimefun.cscorelib2.materials.MaterialCollections;
 
 public final class DiamondBlockPillarSurprise implements Surprise {
 
@@ -20,7 +20,7 @@ public final class DiamondBlockPillarSurprise implements Surprise {
     @Override
     public void activate(Random random, Player p, Location l) {
         for (int i = 0; i < 8; i++) {
-            l.getWorld().spawnFallingBlock(l.clone().add(0.5, (i + 1) * 4.0, 0.5), MaterialCollections.getAllTerracottaColors().get(i).createBlockData());
+            l.getWorld().spawnFallingBlock(l.clone().add(0.5, (i + 1) * 4.0, 0.5), ColoredMaterial.TERRACOTTA.get(i).createBlockData());
         }
 
         l.getWorld().spawnFallingBlock(l.clone().add(0.5, 9 * 4.0, 0.5), Material.DIAMOND_BLOCK.createBlockData());
